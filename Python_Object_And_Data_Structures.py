bcd0b8d9a6d997b1094025c43cf347a2d1374b3e
Jack Gavardari <91555053+jackgavardari@users.noreply.github.com>
2023-08-10 02:49:14 +0100

    Create python_object_and_data_structures.py

---
+++ Python_Object_And_Data_Structures.py
@@ -0,0 +1,7 @@
+# In this section, I will cover the use of numbers in Python and hot to use them.
+# We will cover the following topics:
+# 1. Types of numbers in python
+# 2. Basic Arithmetic
+# 3. The differences between classic division and floor division
+# 4. Object Assignment in Python
+
